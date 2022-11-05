f1b1cf4303cdb155563893d96589e01b124b5fbd
Profas132 <Profas117@gmail.com>
2022-11-05 13:30:13 +0700

    DialogLogic1
    13:30 5.11.22

---
+++ Assets/Script/Dialogs/DialogText.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class DialogText
+{
+    public string dialogText;
+    public int indexOfItems;
+    public string nameOfItem;
+}
--- Assets/Script/NPS/NPSDialog.cs
+++ Assets/Script/NPS/NPSDialog.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class NPSDialog : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI text; 
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private DialogTextGen dialogTextGen;
+    private DialogText currentText;
 
     void Start()
     {
-        text.text = "Эй сталкер, Я тебя спас, но в благородство играть не буду";
+        currentText = dialogTextGen.Ganerate();
+
+        //text.text = "Эй сталкер, Я тебя спас, но в благородство играть не буду";
+        text.text = currentText.dialogText;
     }
 
     // Update is called once per frame
